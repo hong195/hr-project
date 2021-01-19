7d1f66dc02c7f4b8a87858ec449dc15afa4b11c2
saodat <saodat.9808@gmail.com>
2021-01-19 23:31:16 +0500

    Added Single Rating for TableChart

--- resources/frontend/admin/src/views/dashboard/components/Graphs/TableChart.vue
+++ resources/frontend/admin/src/views/dashboard/components/Graphs/TableChart.vue
@@ -1,34 +1,44 @@
 <template>
-  <v-data-table
-    :items="items"
-    :headers="headers"
-    :loading="isLoading"
-    :sort-by="['rating.scored']"
-    :sort-desc="[true]"
-  >
-    <template v-slot:item.rating="{ item }">
-      <tr>
-        <td v-if="item.rating">
-          <v-btn :color="getColor(item.rating.scored)"
-                 rounded
-                 class="rating__btn"
-                 depressed
-          >
-            <span style="color: white;">{{ `${item.rating.scored}/${item.rating.out_of}` }}</span>
-          </v-btn>
-        </td>
-        <td v-else>
-          Нет Рейтинга
-        </td>
-      </tr>
-    </template>
-  </v-data-table>
+  <div>
+    <v-data-table
+      :items="items"
+      :headers="headers"
+      :loading="isLoading"
+      :sort-by="['rating.scored']"
+      :sort-desc="[true]"
+    >
+      <template v-slot:item.rating="{ item }">
+        <tr>
+          <td v-if="item.rating">
+            <v-btn :color="getColor(item.rating.scored)"
+                   rounded
+                   class="rating__btn"
+                   depressed
+                   @click="getRating(item.rating.id)"
+            >
+              <span style="color: white;">{{ `${item.rating.scored}/${item.rating.out_of}` }}</span>
+            </v-btn>
+          </td>
+          <td v-else>
+            Нет Рейтинга
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+    <single-user-rating
+      :show-dialog="dialog"
+      :rating-id="ratingId"
+      @close-dialog="dialog = false"
+    />
+  </div>
 </template>
 
 <script>
   import RatingColor from '@/views/dashboard/components/mixins/RatingColor'
+  import SingleUserRating from '@/views/dashboard/pages/ratings/SingleUserRating'
   export default {
     name: 'TableChart',
+    components: { SingleUserRating },
     mixins: [RatingColor],
     props: {
       items: {
@@ -41,6 +51,8 @@
     },
     data () {
       return {
+        dialog: false,
+        ratingId: null,
         headers: [
           {
             text: 'Аптека',
@@ -52,6 +64,12 @@
           },
         ],
       }
+    },
+    methods: {
+      getRating (ratingId) {
+        this.ratingId = ratingId
+        this.dialog = true
+      },
     },
   }
 </script>
